8fe1c932f36a3313cad82d875e06b172ad4a7dac
Maeci <contact@maecefischer.ch>
2018-12-17 21:53:03 +0100

    installed doctrine

--- src/Api/ApiV1Bundle/Controller.php
+++ src/Api/ApiV1Bundle/Controller.php
@@ -2,6 +2,8 @@
 
 namespace App\Api\ApiV1Bundle;
 
+use App\Packages\Common\Application\Command\Validation\MessageBag;
+use App\Packages\Common\Application\HandlerResponse\ValidationErrorResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,20 +12,21 @@ use App\Packages\Common\Application\CommandBus;
 
 final class Controller extends AbstractController
 {
-    private $router;
+    private $responseFactory;
     private $commandBus;
 
-    public function __construct(ResponseFactory $router, CommandBus $commandBus)
+    public function __construct(ResponseFactory $responseFactory, CommandBus $commandBus)
     {
-        $this->router = $router;
+        $this->responseFactory = $responseFactory;
         $this->commandBus = $commandBus;
     }
 
     public function handle(string $path): Response
     {
+        $handlerResponse = new ValidationErrorResponse(new MessageBag(), new MessageBag());
         //$handlerResponse = $this->commandBus->handle();
         $request = Request::createFromGlobals();
-        $response = $this->router->handle($path, $request);
+        $response = $this->responseFactory->createFromHandlerResponse($request, $handlerResponse);
         return $response;
     }
 }
