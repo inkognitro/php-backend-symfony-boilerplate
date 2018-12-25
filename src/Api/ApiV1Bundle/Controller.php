7113eeaa6a163696d00285fc721de88f5778905e
Maeci <contact@maecefischer.ch>
2018-12-25 18:35:58 +0100

    test preparation doctrine

--- src/Api/ApiV1Bundle/Controller.php
+++ src/Api/ApiV1Bundle/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Api\ApiV1Bundle;
 
+use App\Packages\Authentication\Application\UserFactory;
 use App\Packages\Common\Application\Command\Validation\MessageBag;
 use App\Packages\Common\Application\HandlerResponse\ValidationErrorResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,15 +15,20 @@ final class Controller extends AbstractController
 {
     private $responseFactory;
     private $commandBus;
+    private $authUserFactory;
 
-    public function __construct(ResponseFactory $responseFactory, CommandBus $commandBus)
+    public function __construct(ResponseFactory $responseFactory, CommandBus $commandBus, UserFactory $authUserFactory)
     {
         $this->responseFactory = $responseFactory;
         $this->commandBus = $commandBus;
+        $this->authUserFactory = $authUserFactory;
     }
 
     public function handle(string $path): Response
     {
+        $user = $this->authUserFactory->createFromUserId('287d6446-af61-4451-bc60-85ea545e53b6');
+        die(print_r($user));
+
         $handlerResponse = new ValidationErrorResponse(new MessageBag(), new MessageBag());
         //$handlerResponse = $this->commandBus->handle();
         $request = Request::createFromGlobals();
--- src/Packages/Authentication/Infrastructure/DbalUserByIdQuery.php
+++ src/Packages/Authentication/Infrastructure/DbalUserByIdQuery.php
@@ -3,16 +3,29 @@
 namespace App\Packages\Authentication\Infrastructure;
 
 use App\Packages\Authentication\Application\UserByIdQuery;
+use Doctrine\DBAL\Connection;
 
 final class DbalUserByIdQuery implements UserByIdQuery
 {
+    private $connection;
+
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
     public function execute(string $userId): ?array
     {
-        //todo: implement database query
-        return [
-            'id' => $userId,
-            'role' => 'user',
-            'languageId' => 'en'
-        ];
+        $queryBuilder = $this->connection->createQueryBuilder();
+        $queryBuilder->addSelect('id');
+        $queryBuilder->addSelect('role');
+        $queryBuilder->addSelect('languageId');
+        $queryBuilder->from('users');
+        $queryBuilder->andWhere("id = {$queryBuilder->createNamedParameter($userId)}");
+        $row = $queryBuilder->execute()->fetch();
+        if(!$row) {
+            return null;
+        }
+        return $row;
     }
 }
